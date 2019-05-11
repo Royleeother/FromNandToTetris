6dd15254b2bc44c26438a8dfc2aa0a2dec896d0a
Roy Lee <Li-jinquan@outlook.com>
2019-05-11 18:49:55 +0800

    complete fill, hard hard

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,78 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// define screen pointer (16384)
+@SCREEN
+D=A
+@pointer
+M = D 
+
+// max screen
+//@KBD  
+@24576
+D = A 
+@maxscreen
+M = D 
+
+(RUNS) 
+    @KBD    // if keyboard down, jump to fill  
+    D = M 
+    @FILL   // blacken 
+    D; JGT
+    
+    @UNFILL // whiten 
+    0; JMP  
+    
+
+(UNFILL)
+    // Do nothing if POINTER == SCREEN (pointer back to the starting position)
+    @SCREEN
+    D=A
+    @pointer
+    D=D-M
+    @RUNS
+    D; JEQ
+
+    // Unfill the screen
+    D = 0
+    @pointer
+    A = M 
+    M = D 
+
+    // Decrement pointer
+    D = 1
+    @pointer
+    D = M - D 
+    @pointer 
+    M = D 
+
+    @RUNS
+    0; JMP
+
+(FILL)
+    // Do nothing if the screen is full
+    @maxscreen
+    D = M
+    @pointer
+    D = D - M
+    @RUNS
+    D; JEQ
+
+    // fill in 1 pixel that pointer points each iteration
+    D = -1
+    @pointer
+    A = M 
+    M = D 
+
+    // iterate pointer by 1
+    D = 1
+    @pointer
+    D = D + M 
+    @pointer 
+    M = D 
+
+    @RUNS
+    0; JMP
+
+
